de21a1ce030d4f8ec4039c92226f7304958a4cca
Johannes Maucher <johannes@Johanness-iMac.fritz.box>
2022-03-30 16:05:28 +0200

    mc

--- objectrecogniton/_build/jupyter_execute/preprocessing/04gaussianDerivatives.py
+++ objectrecogniton/_build/jupyter_execute/preprocessing/04gaussianDerivatives.py
@@ -21,9 +21,9 @@
 
 # The univariate Gauss-function is defined as follows:
 # 
-# \begin{equation}
+# $$
 # g_{\sigma,\mu}(x) = \frac{1}{\sqrt{2 \pi} \sigma} \exp \left( - \frac{(x-\mu)^2}{2 \sigma^2}\right),
-# \end{equation}
+# $$
 # 
 # where $\sigma$ is the standard deviation and $\mu$ is the mean. In the context of filtering, the mean is always $\mu=0$, the standard deviation $\sigma$ is a parameter, which determines the width of the filter. In the sequel the mean is implicetly assumed to be $\mu=0.$
 # 
@@ -67,9 +67,9 @@ plt.show()
 
 # In image processing we have discrete signals (the pixels of an image) and discrete filters. For the discrete index $u$, the **discrete Gaussian filter** is defined to be (for $\mu=0$):
 # 
-# \begin{equation}
+# $$
 # g_{\sigma}(u) = \frac{1}{\sqrt{2 \pi} \sigma} \exp \left( - \frac{(u)^2}{2 \sigma^2}\right),
-# \end{equation}
+# $$
 # 
 # For discrete Gauss-filters, u is an integer in the range
 # 
@@ -81,9 +81,9 @@ plt.show()
 # 
 # The output of the discrete Gaussian filtering, if applied to the discrete signal $f(i)$ is than
 # 
-# \begin{equation}
+# $$
 # h(i) = \sum\limits_{u=-z}^{z} \frac{1}{\sqrt{2 \pi} \sigma} \exp \left( - \frac{u^2}{2 \sigma^2}\right) f(i-u)
-# \end{equation}
+# $$
 # 
 # Below, two discrete Gauss functions with different $\sigma$ are plotted: 
 
@@ -141,22 +141,22 @@ fo3=ndi.gaussian_filter1d(x1, sigma=1.5, output=np.float64, mode='nearest')
 # 
 # The 1st order derivative of the zero mean Gaussian function
 # 
-# \begin{equation}
+# $$
 # g_{\sigma}(x) = \frac{1}{\sqrt{2 \pi} \sigma} \exp \left( - \frac{x^2}{2 \sigma^2}\right)
-# \end{equation}
+# $$
 # 
 # can easily be calcultated to be
 # 
 # 
-# \begin{equation}
+# $$
 # g'_{\sigma}(x)=\frac{\partial g_{\sigma}(x)}{\partial x}= -\frac{x}{\sigma^2} \frac{1}{\sqrt{2 \pi} \sigma} \exp \left( - \frac{x^2}{2 \sigma^2}\right) = -\frac{x}{\sigma^2} g_{\sigma}(x),
-# \end{equation}
+# $$
 # 
 # And the 2nd order derivative is
 # 
-# \begin{equation}
+# $$
 # g''_{\sigma}(x)=\frac{\partial^2 g_{\sigma}(x)}{\partial x^2}=\left(\frac{x^2}{\sigma^4} - \frac{1}{\sigma^2} \right) \frac{1}{\sqrt{2 \pi} \sigma} \exp \left( - \frac{x^2}{2 \sigma^2}\right) = \left(\frac{x^2}{\sigma^4} - \frac{1}{\sigma^2} \right) g_{\sigma}(x).
-# \end{equation}
+# $$
 
 # Below, the gaussian function and it's 1st and 2nd order derivative are visualized:
 
@@ -189,9 +189,9 @@ plt.title("2nd order derivative of Gaussian Filter")
 # 
 # In [section filtering](02filtering.ipynb) it was shown that the gradient $\frac{\partial f}{\partial x}$ of a 1-dimensional discrete signal $f$ can be calculated by convolving the signal with the filter $h=(1,0,-1)$:
 # 
-# \begin{equation}
+# $$
 # \frac{\partial f}{\partial x}=h*f
-# \end{equation} 
+# $$
 # 
 # By comparing the form of filter $h$, with the first derivative of the Gaussian, it becomes obvious, that the first derivative of the Gaussian is a smoothed form of $h$. **Filtering a signal $f$ with a Gaussian and then calculating its gradient is the same as filtering the signal $f$ with the first order derivative of the Gaussian.**
 
@@ -200,9 +200,9 @@ plt.title("2nd order derivative of Gaussian Filter")
 # 
 # A 2-dimensional continous Gaussian filter (again $\mu=0$) is defined as follows:
 # 
-# \begin{equation}
+# $$
 # G_{\sigma}(x,y) = \frac{1}{2 \pi \sigma^2} \exp \left( - \frac{x^2+y^2}{2 \sigma^2}\right)
-# \end{equation}
+# $$
 # 
 # The following function `gauss2d(mu,sigma,order)` plots either 
 # * the 2-dimensional Gauss-function, if `order=[0,0]`
@@ -280,17 +280,17 @@ z = gauss2d(MU, SIGMA, order=[0,0])
 
 # The discrete form of the Gaussian filter is
 # 
-# \begin{equation}
+# $$
 # G_{\sigma}(u,v)  =  \frac{1}{2 \pi \sigma^2} \exp \left( - \frac{u^2+v^2}{2 \sigma^2}\right),
-# \end{equation}
+# $$
 # 
 # where $u$ and $v$ are integers in the range of $[-z, \ldots, z]$, and $z=\lceil 3\sigma \rceil$.
 # 
 # The convolution of a discrete 2-dimensional signal $F$ with the 2-dimensional Gaussian filter $G$ is then:
 # 
-# \begin{equation}
+# $$
 # H(i,j) = \sum\limits_{u=-k}^{k} \sum\limits_{v=-k}^{k} \frac{1}{2 \pi \sigma^2} \exp \left( - \frac{u^2+v^2}{2 \sigma^2}\right) F(i-u,j-v).
-# \end{equation}
+# $$
 
 # Next, 2-dimensional Gaussian filtering is demonstrated. For this we define
 # a 2-dimensional signal $X1$ of shape (10,10) as a numpy-array. The signal values are 1 (white) in the (4,4)-center region and 0 (black) elsewhere.
@@ -334,10 +334,13 @@ plt.title('Image X1 filtered by Gaussian filter')
 # 
 # For the 2-dimensional Gaussian $G_{\sigma}(x,y)$ the 1st order partial derivatives are
 # 
-# \begin{eqnarray}
-# \frac{\partial G_{\sigma}(x,y)}{\partial x} & = & -\frac{x}{\sigma^2} G_{\sigma}(x,y) \\
-# \frac{\partial G_{\sigma}(x,y)}{\partial y} & = & -\frac{y}{\sigma^2} G_{\sigma}(x,y)
-# \end{eqnarray}
+# $$
+# \frac{\partial G_{\sigma}(x,y)}{\partial x}  =  -\frac{x}{\sigma^2} G_{\sigma}(x,y) 
+# $$
+# 
+# $$
+# \frac{\partial G_{\sigma}(x,y)}{\partial y}  =  -\frac{y}{\sigma^2} G_{\sigma}(x,y)
+# $$
 # 
 # The 1st order derivatives are visualized below:
 
@@ -444,17 +447,17 @@ plt.show()
 # 
 # - The **Laplacian Operator $\nabla^2 F$** is the undirected 2nd derivative of a 2-dimensional image $F$:
 # 
-# \begin{equation}
+# $$
 # \nabla^2 F = \frac{\partial^2 F}{\partial x^2}+\frac{\partial^2 F}{\partial y^2}
-# \end{equation}
+# $$
 # 
 # - The second order derivatives of the 2-dim Gaussian filter are defined in {eq}`2nd_derivative_gauss`
 # 
 # - Smoothing an image $F$ with a Gaussian and then taking its Laplacian operator is equivalent to convolving the image $F$ with the **Laplacian of Gaussian (LoG) Filter**:
 # 
-# \begin{equation}
+# $$
 # \nabla^2 G_{\sigma}(x,y)=\left(\frac{x^2+y^2}{\sigma^4} - \frac{2}{\sigma^2}\right) G_{\sigma}(x,y) 
-# \end{equation} 
+# $$
 
 # In[22]:
 
